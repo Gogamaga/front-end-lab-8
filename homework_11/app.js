5be41deac3a742aabf79391f03ee8508ff81065d
Gogamaga <jextailor@gmail.com>
2018-02-27 20:01:29 +0200

    fix HM_11

--- homework_11/app.js
+++ homework_11/app.js
@@ -1,43 +1,31 @@
-var rootNode = document.getElementById("root");
+const rootNode = document.getElementById("root");
 const div = document.createElement("div");
 
 function createTree(array, parentNode) {
-    array.forEach(element => {
+    array.forEach(({ title, folder, children }) => {
         const ul = document.createElement("ul");
         const li = document.createElement("li");
         const p = document.createElement("p");
-        if (element.folder) {
-            p.innerHTML = element.title;
-            li.appendChild(p);
-            p.className = "folder folder_close";
-            ul.appendChild(li);
-            parentNode.appendChild(ul);
-            if (element.children) {
-                return createTree(element.children, li);
-            } else {
-                const liEmpty = document.createElement("li");
-                const ulempty = document.createElement("ul");
-                liEmpty.innerHTML = "folder is empty";
-                ulempty.appendChild(liEmpty);
-                li.appendChild(ulempty);
-            }
-        } else {
-            p.innerHTML = element.title;
-            li.appendChild(p);
-            p.className = "file";
-            ul.appendChild(li);
-            parentNode.appendChild(ul);
-            element.children && createTree(element.children, li);
+        p.innerHTML = title;
+        li.appendChild(p);
+        p.className = folder ? "folder folder_close" : "file";
+        ul.appendChild(li);
+        parentNode.appendChild(ul);
+        if (children) {
+            return createTree(children, li);
+        } else if (!children && folder) {
+            const liEmpty = document.createElement("li");
+            const ulempty = document.createElement("ul");
+            liEmpty.innerHTML = "folder is empty";
+            ulempty.appendChild(liEmpty);
+            li.appendChild(ulempty);
         }
     });
     return parentNode;
 }
 
 rootNode.addEventListener("click", ({ target }) => {
-    if (/folder/.test(target.className)) {
-        target.classList.toggle("folder_close");
-        target.classList.toggle("folder_open");
-    }
+    /folder/.test(target.className) && target.classList.toggle("folder_close");
+    target.classList.toggle("folder_open");
 });
-
 rootNode.appendChild(createTree(structure, div));
